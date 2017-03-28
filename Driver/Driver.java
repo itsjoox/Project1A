34f67b34db0c3fe784f7c9a3642414ceffe8cec9
itsjoox <645844892@qq.com>
2017-03-28 17:18:37 -0500

    update driver

--- Driver/Driver.java
+++ Driver/Driver.java
@@ -1,5 +1,34 @@
 package Driver;
 
-public class Driver {
-
-}
+import java.sql.Connection;  
+import java.sql.DriverManager;  
+import java.sql.Statement;  
+  
+public class Driver {  
+    public static void main(String args[]) {  
+        Connection c = null;  
+        Statement stmt = null;  
+        try {  
+            Class.forName("org.postgresql.Driver");  
+  
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "ricedb", "12345678");
+            System.out.println("Opened database successfully");  
+            c.setAutoCommit(false); 
+//            stmt = c.createStatement();   
+//            String sql = "CREATE TABLE STUDENTS " +   
+//                         "(ID TEXT PRIMARY KEY     NOT NULL ," +   
+//                         " NAME            TEXT    NOT NULL, " +   
+//                         " SEX             TEXT    NOT NULL, " +   
+//                         " AGE             TEXT    NOT NULL)";   
+//            stmt.executeUpdate(sql);   
+//            System.out.println("Table created successfully");  
+//              
+//            stmt.close();  
+            c.commit();  
+            c.close();  
+        } catch (Exception e) {  
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());  
+            System.exit(0);  
+        }  
+    }  
+}  
